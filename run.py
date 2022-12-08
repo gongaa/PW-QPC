2ed5269728664b4d9741d0b1bd68cce54aea8b07
gongaa <gonga@student.ethz.ch>
2022-12-08 23:05:51 +0100

    add run script, add plot script

---
+++ run.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+import numpy as np
+
+N = input('Enter N:')
+K = input('Enter K:')
+l = input('Enter list size:')
+pz_min = input('Enter a range of pz, pz_min: ')
+pz_max = input('Enter a range of pz, pz_max (not inclusive): ')
+s = input('Enter random seed:')
+n = input('Enter number of samples: ')
+while True:
+    con = input('Enter construction method (PW, HPW, RM): ')
+    if con in ['PW', 'HPW', 'RM']:
+        break
+exact = input("Enter exact interval (0 to not use exact): ")
+euler = input("Running on Euler? 'y' or 'n': ") == 'y'
+if euler:
+    runtime = input("Enter runtime: ")
+
+path = 'logs'
+if not os.path.exists(path):    
+    try:
+        os.mkdir(path)
+    except OSError as error:
+        print(error)   
+
+dir = 'N'+N+'_K'+K+'_l'+l+'_s'+s+'_n'+n+'_exact'+exact+'_'+con
+try:
+    os.mkdir(path+'/'+dir)
+except OSError as error:
+    print(error)  
+
+print("Your results will be saved under the directory logs/" + dir)
+
+for pz in np.arange(float(pz_min), float(pz_max), 0.01):
+    cmd = "./build/apps/program -N "+N+" -K "+K+" -l "+l+" -seed "+s+" -n "+n+" -exact "+exact+ " -con "+con+" -pz "+str(pz)
+    if euler:
+        cmd = 'bsub -W ' + runtime + ':00 "'+ cmd + '"'
+    dest = "logs/" + dir + "/pz" + str(int(pz*100)) + ".log"
+    process = subprocess.Popen(cmd.split(), stderr=open(dest, 'w'))
+    print(cmd)

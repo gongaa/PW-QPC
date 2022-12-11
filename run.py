b5b8412ba41c68e6e4538669e4b9d9ca593df7b7
gongaa <gonga@student.ethz.ch>
2022-12-11 22:09:59 +0100

    add HPW_vs_PW.html which contains all the simulation results

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ K = input('Enter K:')
 l = input('Enter list size:')
 pz_min = input('Enter a range of pz, pz_min: ')
 pz_max = input('Enter a range of pz, pz_max (not inclusive): ')
-s = input('Enter random seed:')
+seed = input('Enter random seed:')
 n = input('Enter number of samples: ')
 while True:
     con = input('Enter construction method (PW, HPW, RM): ')
@@ -25,18 +25,22 @@ if not os.path.exists(path):
     except OSError as error:
         print(error)   
 
-dir = 'N'+N+'_K'+K+'_l'+l+'_s'+s+'_n'+n+'_exact'+exact+'_'+con
-try:
-    os.mkdir(path+'/'+dir)
-except OSError as error:
-    print(error)  
+def run_exp(s):
+    dir = 'N'+N+'_K'+K+'_l'+l+'_s'+s+'_n'+n+'_exact'+exact+'_'+con
+    try:
+        os.mkdir(path+'/'+dir)
+    except OSError as error:
+        print(error)  
 
-print("Your results will be saved under the directory logs/" + dir)
+    print("Your results will be saved under the directory logs/" + dir)
 
-for pz in np.arange(float(pz_min), float(pz_max), 0.01):
-    cmd = "./build/apps/program -N "+N+" -K "+K+" -l "+l+" -seed "+s+" -n "+n+" -exact "+exact+ " -con "+con+" -pz "+str(round(pz,2))
-    dest = "logs/" + dir + "/pz" + str(int(round(pz,2)*100)) + ".log"
-    if euler:
-        process = subprocess.Popen(['bsub', '-W', runtime+':00', cmd+' &> '+dest])
-    else:
-        process = subprocess.Popen(cmd.split(), stderr=open(dest, 'w'))
+    for pz in np.arange(float(pz_min), float(pz_max), 0.01):
+        cmd = "./build/apps/program -N "+N+" -K "+K+" -l "+l+" -seed "+s+" -n "+n+" -exact "+exact+ " -con "+con+" -pz "+str(round(pz,2))
+        dest = "logs/" + dir + "/pz" + str(int(round(pz,2)*100)) + ".log"
+        if euler:
+            process = subprocess.Popen(['bsub', '-W', runtime+':00', cmd+' &> '+dest])
+        else:
+            process = subprocess.Popen(cmd.split(), stderr=open(dest, 'w'))
+
+for s in seed.split(','):
+    run_exp(s)
